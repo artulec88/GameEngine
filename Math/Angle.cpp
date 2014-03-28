5061c4db91ef4f5329e0f6773c44028babe9e863
artulec88 <artur.osesik@gmail.com>
2014-03-28 17:18:56 +0100

    Created implementation file for Angle class

---
+++ Math/Angle.cpp
@@ -0,0 +1,19 @@
+#include "Angle.h"
+
+using namespace Math;
+
+Angle::Angle(Real angle, bool isInDegrees /* = true */) :
+  angle(angle),
+  isInDegrees(isInDegrees)
+{
+}
+
+Math::Real Angle::GetAngleInDegrees() const
+{
+  return (isInDegrees) ? angle : ToDeg(angle);
+}
+
+Math::Real Angle::GetAngleInRadians() const
+{
+  return (isInDegrees) ? ToRad(angle) : angle;
+}
