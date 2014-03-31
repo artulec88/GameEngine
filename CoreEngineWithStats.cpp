f9ef1c1dc599cadc9c3063c6f99ba0a7facf6458
artulec88 <artur.osesik@gmail.com>
2014-03-31 14:59:53 +0200

    Extended stats are displayed to the user
    Statistics contain detailed time information about each part / region of the main loop.

--- CoreEngineWithStats.cpp
+++ CoreEngineWithStats.cpp
@@ -86,10 +86,33 @@ void CoreEngine::Start()
 }
 
 long countStats1 = 0;
+double minTime1 = 9999999.99;
+double maxTime1 = 0.0;
 double timeSum1 = 0.0;
+
 long countStats2 = 0;
+double minTime2 = 9999999.99;
+double maxTime2 = 0.0;
 double timeSum2 = 0.0;
+
+long countStats2_1 = 0;
+double minTime2_1 = 9999999.99;
+double maxTime2_1 = 0.0;
+double timeSum2_1 = 0.0;
+
+long countStats2_2 = 0;
+double minTime2_2 = 9999999.99;
+double maxTime2_2 = 0.0;
+double timeSum2_2 = 0.0;
+
+long countStats2_3 = 0;
+double minTime2_3 = 9999999.99;
+double maxTime2_3 = 0.0;
+double timeSum2_3 = 0.0;
+
 long countStats3 = 0;
+double minTime3 = 9999999.99;
+double maxTime3 = 0.0;
 double timeSum3 = 0.0;
 
 void CoreEngine::Stop()
@@ -100,16 +123,18 @@ void CoreEngine::Stop()
 		return;
 	}
 	
-	/* ==================== printing stats begin ==================== */
-	stdlog(Debug, LOGPLACE, "The first region was processed %d times, which took exactly %.4f [ms]. The average time is %.4f [us]", countStats1, timeSum1, 1000.0 * timeSum1 / countStats1);
-	stdlog(Debug, LOGPLACE, "The second region was processed %d times, which took exactly %.4f [ms]. The average time is %.4f [us]", countStats2, timeSum2, 1000.0 * timeSum2 / countStats2);
-	stdlog(Debug, LOGPLACE, "The third region was processed %d times, which took exactly %.4f [ms]. The average time is %.4f [us]", countStats3, timeSum3, 1000.0 * timeSum3 / countStats3);
-	/* ==================== printing stats end ==================== */
-	
-	
 	isRunning = false;
 	ASSERT(!isRunning);
 	stdlog(Notice, LOGPLACE, "The core engine has stopped");
+	
+	/* ==================== Printing stats begin ==================== */
+	stdlog(Debug, LOGPLACE, "The region #1 (Time calculating) was processed %d times, which took exactly %.4f [ms]. The average time=%.4f [us],\t max time=%.4f [us],\t min time=%.4f [us]", countStats1, timeSum1, 1000.0 * timeSum1 / countStats1, 1000.0 * minTime1, 1000.0 * maxTime1);
+	stdlog(Debug, LOGPLACE, "The region #2 was processed %d times, which took exactly %.4f [ms]. The average time=%.4f [us],\t max time=%.4f [us],\t min time=%.4f [us]", countStats2, timeSum2, 1000.0 * timeSum2 / countStats2, 1000.0 * minTime2, 1000.0 * maxTime2);
+	stdlog(Debug, LOGPLACE, "\t The region #2_1 (Polling events) was processed %d times, which took exactly %.4f [ms]. The average time=%.4f [us],\t max time=%.4f [us],\t min time=%.4f [us]", countStats2_1, timeSum2_1, 1000.0 * timeSum2_1 / countStats2_1, 1000.0 * minTime2_1, 1000.0 * maxTime2_1);
+	stdlog(Debug, LOGPLACE, "\t The region #2_2 (Game input processing) was processed %d times, which took exactly %.4f [ms]. The average time=%.4f [us],\t max time=%.4f [us],\t min time=%.4f [us]", countStats2_2, timeSum2_2, 1000.0 * timeSum2_2 / countStats2_2, 1000.0 * minTime2_2, 1000.0 * maxTime2_2);
+	stdlog(Debug, LOGPLACE, "\t The region #2_3 (Game updating) was processed %d times, which took exactly %.4f [ms]. The average time=%.4f [us],\t max time=%.4f [us],\t min time=%.4f [us]", countStats2_3, timeSum2_3, 1000.0 * timeSum2_3 / countStats2_3, 1000.0 * minTime2_3, 1000.0 * maxTime2_3);
+	stdlog(Debug, LOGPLACE, "The region #3 (Rendering) was processed %d times, which took exactly %.4f [ms]. The average time=%.4f [us],\t max time=%.4f [us],\t min time=%.4f [us]", countStats3, timeSum3, 1000.0 * timeSum3 / countStats3, 1000.0 * minTime3, 1000.0 * maxTime3);
+	/* ==================== Printing stats end ==================== */
 }
 
 void CoreEngine::Run()
@@ -132,6 +157,7 @@ void CoreEngine::Run()
 
 	while (isRunning)
 	{
+		/* ==================== REGION #1 begin ====================*/
 		clock_t begin = clock();
 		bool isRenderRequired = false;
 
@@ -156,8 +182,19 @@ void CoreEngine::Run()
 #endif
 		clock_t end = clock();
 		countStats1++;
-		timeSum1 += static_cast<double>(1000.0 * (end - begin)) / CLOCKS_PER_SEC; // in [ms]
+		double elapsedTime = static_cast<double>(1000.0 * (end - begin)) / CLOCKS_PER_SEC; // in [ms]
+		if (elapsedTime > maxTime1)
+		{
+			maxTime1 = elapsedTime;
+		}
+		else if (elapsedTime < minTime1)
+		{
+			minTime1 = elapsedTime;
+		}
+		timeSum1 += elapsedTime;
+		/* ==================== REGION #1 end ====================*/
 
+		/* ==================== REGION #2 begin ====================*/
 		begin = clock();
 		while (unprocessingTime > frameTime)
 		{
@@ -167,16 +204,76 @@ void CoreEngine::Run()
 				Stop();
 				return;
 			}
+			/* ==================== REGION #2_1 begin ====================*/
+			clock_t innerBegin = clock();
 			PollEvents();
+			clock_t innerEnd = clock();
+			countStats2_1++;
+			elapsedTime = static_cast<double>(1000.0 * (end - begin)) / CLOCKS_PER_SEC; // in [ms]
+			if (elapsedTime > maxTime2_1)
+			{
+				maxTime2_1 = elapsedTime;
+			}
+			else if (elapsedTime < minTime2_1)
+			{
+				minTime2_1 = elapsedTime;
+			}
+			timeSum2_1 += elapsedTime;
+			/* ==================== REGION #2_1 end ====================*/
+			
+			/* ==================== REGION #2_2 begin ====================*/
+			innerBegin = clock();
 			game->Input(frameTime);
+			innerEnd = clock();
+			countStats2_2++;
+			elapsedTime = static_cast<double>(1000.0 * (end - begin)) / CLOCKS_PER_SEC; // in [ms]
+			if (elapsedTime > maxTime2_2)
+			{
+				maxTime2_2 = elapsedTime;
+			}
+			else if (elapsedTime < minTime2_2)
+			{
+				minTime2_2 = elapsedTime;
+			}
+			timeSum2_2 += elapsedTime;
+			/* ==================== REGION #2_2 end ====================*/
+			
 			//Input::Update();
+			
+			/* ==================== REGION #2_3 begin ====================*/
+			innerBegin = clock();
 			game->Update(frameTime);
+			innerEnd = clock();
+			countStats2_3++;
+			elapsedTime = static_cast<double>(1000.0 * (end - begin)) / CLOCKS_PER_SEC; // in [ms]
+			if (elapsedTime > maxTime2_3)
+			{
+				maxTime2_3 = elapsedTime;
+			}
+			else if (elapsedTime < minTime2_3)
+			{
+				minTime2_3 = elapsedTime;
+			}
+			timeSum2_3 += elapsedTime;
+			/* ==================== REGION #2_3 end ====================*/
+			
 			unprocessingTime -= frameTime;
 		}
 		end = clock();
 		countStats2++;
-		timeSum2 += static_cast<double>(1000.0 * (end - begin)) / CLOCKS_PER_SEC; // in [ms]
+		elapsedTime = static_cast<double>(1000.0 * (end - begin)) / CLOCKS_PER_SEC; // in [ms]
+		if (elapsedTime > maxTime2)
+		{
+			maxTime2 = elapsedTime;
+		}
+		else if (elapsedTime < minTime2)
+		{
+			minTime2 = elapsedTime;
+		}
+		timeSum2 += elapsedTime; // in [ms]
+		/* ==================== REGION #2 end ====================*/
 		
+		/* ==================== REGION #3 begin ====================*/
 		begin = clock();
 		if (isRenderRequired)
 		{
@@ -201,7 +298,17 @@ void CoreEngine::Run()
 		}
 		end = clock();
 		countStats3++;
-		timeSum3 += static_cast<double>(1000.0 * (end - begin)) / CLOCKS_PER_SEC; // in [ms]
+		elapsedTime = static_cast<double>(1000.0 * (end - begin)) / CLOCKS_PER_SEC; // in [ms]
+		if (elapsedTime > maxTime3)
+		{
+			maxTime3 = elapsedTime;
+		}
+		else if (elapsedTime < minTime3)
+		{
+			minTime3 = elapsedTime;
+		}
+		timeSum3 += elapsedTime; // in [ms]
+		/* ==================== REGION #3 end ====================*/
 	}
 }
 
