620c4ce5790dbeba8b4eb82f19619a07818f519a
artulec88 <artur.osesik@gmail.com>
2014-11-20 23:28:21 +0100

    Minor Renderer class refactorization

--- Rendering/Renderer.h
+++ Rendering/Renderer.h
@@ -30,36 +30,6 @@ private:
 	static const Math::Matrix4D s_biasMatrix;
 /* ==================== Static variables end ==================== */
 
-/* ==================== Non-static member variables begin ==================== */
-private:
-	GLFWwindow* window;
-	GLuint framebuffer;
-	GLuint vao; // vertex array id
-	bool isFullscreen;
-	bool isMouseEnabled;
-	Math::Vector3D ambientLight;
-	BaseLight* currentLight;
-	unsigned int currentCameraIndex;
-	Camera* currentCamera;
-	
-	Camera	altCamera; // alternative camera for shadow mapping, rendering to texture etc.
-	GameNode* altCameraNode;
-	Material* planeMaterial;
-	Transform planeTransform;
-	Mesh* planeMesh;
-	Texture* tempTarget;
-	
-	Shader* defaultShader;
-	Shader* shadowMapShader;
-	int shadowMapWidth;
-	int shadowMapHeight;
-	std::vector<BaseLight*> lights;
-	std::vector<Camera*> cameras;
-	std::map<std::string, unsigned int> samplerMap;
-	Math::Matrix4D lightMatrix;
-
-/* ==================== Non-static member variables end ==================== */
-
 /* ==================== Constructors and destructors begin ==================== */
 public:
 	Renderer(int width, int height, std::string title);
@@ -102,6 +72,36 @@ private:
 	void InitGlew() const;
 	void SetGlfwCallbacks();
 /* ==================== Non-static, non-virtual member functions end ==================== */
+
+/* ==================== Non-static member variables begin ==================== */
+private:
+	GLFWwindow* window;
+	GLuint framebuffer;
+	GLuint vao; // vertex array id
+	bool isFullscreen;
+	bool isMouseEnabled;
+	Math::Vector3D ambientLight;
+	BaseLight* currentLight;
+	unsigned int currentCameraIndex;
+	Camera* currentCamera;
+	
+	Camera	altCamera; // alternative camera for shadow mapping, rendering to texture etc.
+	GameNode* altCameraNode;
+	Material* planeMaterial;
+	Transform planeTransform;
+	Mesh* planeMesh;
+	Texture* tempTarget;
+	
+	Shader* defaultShader;
+	Shader* shadowMapShader;
+	int shadowMapWidth;
+	int shadowMapHeight;
+	std::vector<BaseLight*> lights;
+	std::vector<Camera*> cameras;
+	std::map<std::string, unsigned int> samplerMap;
+	Math::Matrix4D lightMatrix;
+
+/* ==================== Non-static member variables end ==================== */
 }; /* end class Renderer */
 
 } /* end namespace Rendering */
