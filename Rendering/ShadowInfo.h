c1cb72b36a387d30919c208b71418b1316bc95a5
artulec88 <artur.osesik@gmail.com>
2014-11-21 10:42:41 +0100

    Shadow info with information for VSM method
    Bias member variable (used earlier by basic shadow mapping) was no longer required, thus it was removed.

--- Rendering/ShadowInfo.h
+++ Rendering/ShadowInfo.h
@@ -9,13 +9,15 @@ class RENDERING_API ShadowInfo
 /* ==================== Non-static member variables begin ==================== */
 private:
 	Math::Matrix4D projectionMatrix;
-	Math::Real bias;
 	bool flipFacesEnabled;
+	Math::Real shadowSoftness;
+	Math::Real lightBleedingReductionAmount;
+	Math::Real minVariance;
 /* ==================== Non-static member variables end ==================== */
 
 /* ==================== Constructors and destructors begin ==================== */
 public:
-	ShadowInfo(const Math::Matrix4D& projection, Math::Real bias, bool flipFacesEnabled);
+	ShadowInfo(const Math::Matrix4D& projection, bool flipFacesEnabled, Math::Real shadowSoftness, Math::Real lightBleedingReductionAmount, Math::Real minVariance = static_cast<Real>(0.00002f) );
 	virtual ~ShadowInfo();
 private:
 	ShadowInfo(const ShadowInfo& shadowInfo) {}
@@ -25,7 +27,9 @@ private:
 /* ==================== Non-static member functions begin ==================== */
 public:
 	inline Math::Matrix4D GetProjection() const { return projectionMatrix; }
-	inline Math::Real GetBias() const { return bias; }
+	inline Math::Real GetShadowSoftness() const { return shadowSoftness; }
+	inline Math::Real GetMinVariance() const { return minVariance; }
+	inline Math::Real GetLightBleedingReductionAmount() const { return lightBleedingReductionAmount; }
 	inline bool IsFlipFacesEnabled() const { return flipFacesEnabled; }
 /* ==================== Non-static member functions end ==================== */
 }; /* end class ShadowInfo */
