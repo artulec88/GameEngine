a359f03abb3a1b0ab0e93cf6f67f208b8a1892ed
artulec88 <artur.osesik@gmail.com>
2014-11-21 10:46:28 +0100

    Default parameters in ShadowInfo constructor

--- Rendering/ShadowInfo.cpp
+++ Rendering/ShadowInfo.cpp
@@ -5,7 +5,7 @@
 using namespace Rendering;
 using namespace Utility;
 
-ShadowInfo::ShadowInfo(const Math::Matrix4D& projection, bool flipFacesEnabled, Math::Real shadowSoftness, Math::Real lightBleedingReductionAmount, Math::Real minVariance /* = static_cast<Real>(0.00002f) */) :
+ShadowInfo::ShadowInfo(const Math::Matrix4D& projection, bool flipFacesEnabled, Math::Real shadowSoftness /* = 1.0f*/, Math::Real lightBleedingReductionAmount /* = 0.2f */, Math::Real minVariance /* = static_cast<Real>(0.00002f) */) :
 	projectionMatrix(projection),
 	flipFacesEnabled(flipFacesEnabled),
 	shadowSoftness(shadowSoftness),
