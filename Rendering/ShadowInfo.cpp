574f7df2e09b19c0c08669c050d4450242fe7da5
artulec88 <artur.osesik@gmail.com>
2014-11-21 10:44:08 +0100

    ShadowInfo constructor modified

--- Rendering/ShadowInfo.cpp
+++ Rendering/ShadowInfo.cpp
@@ -5,11 +5,24 @@
 using namespace Rendering;
 using namespace Utility;
 
+	Math::Matrix4D projectionMatrix;
+	bool flipFacesEnabled;
+	Math::Real shadowSoftness;
+	Math::Real lightBleedingReductionAmount;
+	Math::Real minVariance;
+/* ==================== Non-static member variables end ==================== */
 
-ShadowInfo::ShadowInfo(const Math::Matrix4D& projection, Math::Real bias, bool flipFacesEnabled) :
+/* ==================== Constructors and destructors begin ==================== */
+public:
+	ShadowInfo(const Math::Matrix4D& projection, bool flipFacesEnabled, Math::Real shadowSoftness, Math::Real lightBleedingReductionAmount, Math::Real minVariance = static_cast<Real>(0.00002f) );
+
+
+ShadowInfo::ShadowInfo(const Math::Matrix4D& projection, bool flipFacesEnabled, Math::Real shadowSoftness, Math::Real lightBleedingReductionAmount, Math::Real minVariance /* = static_cast<Real>(0.00002f) */) :
 	projectionMatrix(projection),
-	bias(bias),
-	flipFacesEnabled(flipFacesEnabled)
+	flipFacesEnabled(flipFacesEnabled),
+	shadowSoftness(shadowSoftness),
+	lightBleedingReductionAmount(lightBleedingReductionAmount),
+	minVariance(minVariance)
 {
 }
 
