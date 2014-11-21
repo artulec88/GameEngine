1a06868ef8d1bb1f8ba97816411bf9990e98d56a
artulec88 <artur.osesik@gmail.com>
2014-11-21 10:49:47 +0100

    DirectionalLight uses ShadowInfo new constructor

--- Rendering/DirectionalLight.cpp
+++ Rendering/DirectionalLight.cpp
@@ -10,7 +10,7 @@ DirectionalLight::DirectionalLight(const Math::Vector3D& color /* = Math::Vector
 	BaseLight(color, intensity)
 {
 	SetShader(new Shader(GET_CONFIG_VALUE_STR("directionalLightShader", "ForwardDirectional")));
-	ShadowInfo* shadowInfo = new ShadowInfo(Math::Matrix4D::OrtographicProjection(-40, 40, -40, 40, -40, 40), static_cast<Math::Real>(1.0), true);
+	ShadowInfo* shadowInfo = new ShadowInfo(Math::Matrix4D::OrtographicProjection(-40, 40, -40, 40, -40, 40), true);
 	ASSERT(shadowInfo != NULL);
 	SetShadowInfo(shadowInfo);
 }
