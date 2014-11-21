6b43f27716f98add166fb404901046e53f1aea12
artulec88 <artur.osesik@gmail.com>
2014-11-21 10:44:29 +0100

    Fixed the copy-paste error

--- Rendering/ShadowInfo.cpp
+++ Rendering/ShadowInfo.cpp
@@ -5,18 +5,6 @@
 using namespace Rendering;
 using namespace Utility;
 
-	Math::Matrix4D projectionMatrix;
-	bool flipFacesEnabled;
-	Math::Real shadowSoftness;
-	Math::Real lightBleedingReductionAmount;
-	Math::Real minVariance;
-/* ==================== Non-static member variables end ==================== */
-
-/* ==================== Constructors and destructors begin ==================== */
-public:
-	ShadowInfo(const Math::Matrix4D& projection, bool flipFacesEnabled, Math::Real shadowSoftness, Math::Real lightBleedingReductionAmount, Math::Real minVariance = static_cast<Real>(0.00002f) );
-
-
 ShadowInfo::ShadowInfo(const Math::Matrix4D& projection, bool flipFacesEnabled, Math::Real shadowSoftness, Math::Real lightBleedingReductionAmount, Math::Real minVariance /* = static_cast<Real>(0.00002f) */) :
 	projectionMatrix(projection),
 	flipFacesEnabled(flipFacesEnabled),
