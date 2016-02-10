605be99cc11ed18af4a195bdd4a87b6e322edcc2
artulec88 <artulec88@gmail.com>
2016-02-10 14:10:22 +0100

    LookAtComponent now follows the specified entity (it does not have to be the camera).

--- Rendering/LookAtComponent.cpp
+++ Rendering/LookAtComponent.cpp
@@ -1,6 +1,6 @@
 #include "StdAfx.h"
 #include "LookAtComponent.h"
-#include "Transform.h"
+#include "GameNode.h"
 #include "Renderer.h"
 #include "Utility\ILogger.h"
 #include "Math\Quaternion.h"
@@ -8,8 +8,9 @@
 
 using namespace Rendering;
 
-LookAtComponent::LookAtComponent(void) :
+LookAtComponent::LookAtComponent(GameNode* gameNode) :
 	GameComponent(),
+	m_entityToLookAtTransform(gameNode->GetTransform()),
 	renderer(CoreEngine::GetCoreEngine()->GetRenderer())
 {
 }
@@ -23,7 +24,7 @@ void LookAtComponent::Update(Math::Real delta)
 {
 	CHECK_CONDITION(renderer != NULL, Utility::Critical, "Cannot update lookup component due to renderer object being NULL.");
 	//Math::Vector3D currentCameraPos = renderer->GetCurrentCamera().GetTransform().GetPos();
-	Math::Vector3D currentCameraPos = renderer->GetCurrentCamera().GetTransform().GetTransformedPos();
+	Math::Vector3D currentEntityToLookAtPos = m_entityToLookAtTransform.GetTransformedPos();
 
 	//GetTransform().LookAt(currentCameraPos, Math::Vector3D(0, 1, 0));
 
--- Rendering/LookAtComponent.h
+++ Rendering/LookAtComponent.h
@@ -3,17 +3,20 @@
 #include "Rendering.h"
 #include "GameComponent.h"
 #include "IUpdateable.h"
+#include "Transform.h"
 #include "Math\Vector.h"
 
 namespace Rendering
 {
+
+	class GameNode;
 
 // TODO: Inherit from IUpdateable interface.
 class LookAtComponent : public GameComponent, public IUpdateable
 {
 /* ==================== Constructors and destructors begin ==================== */
 public:
-	RENDERING_API LookAtComponent();
+	RENDERING_API LookAtComponent(GameNode* gameNode);
 	RENDERING_API virtual ~LookAtComponent(void);
 /* ==================== Constructors and destructors end ==================== */
 
@@ -24,6 +27,7 @@ public:
 
 /* ==================== Non-static member variables begin ==================== */
 protected:
+	const Transform& m_entityToLookAtTransform;
 	Renderer* renderer;
 /* ==================== Non-static member variables end ==================== */
 }; /* end class LookAtComponent */
