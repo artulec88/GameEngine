78e770b24c0ac115240f71bfce50f98e4045bb23
artulec88 <artulec88@gmail.com>
2016-03-12 17:26:25 +0100

    Physics and Terrain components added.

---
+++ Engine/PhysicsComponent.cpp
@@ -0,0 +1,29 @@
+#include "stdafx.h"
+#include "PhysicsComponent.h"
+#include "Math\Transform.h"
+#include "Math\Quaternion.h"
+
+///* static */ const Math::Real Engine::PhysicsComponent::GRAVITY = 0.001f; // TODO: Don't hard-code any value! Ever!
+
+Engine::PhysicsComponent::PhysicsComponent(Physics::PhysicsObject* physicsObject) :
+	GameComponent(),
+	m_physicsObject(physicsObject)
+{
+}
+
+
+Engine::PhysicsComponent::~PhysicsComponent()
+{
+	SAFE_DELETE(m_physicsObject);
+}
+
+void Engine::PhysicsComponent::Update(Math::Real deltaTime)
+{
+	//m_physicsObject->Integrate(deltaTime);
+}
+
+void Engine::PhysicsComponent::KeyEvent(int key, int scancode, int action, int mods)
+{
+	DEBUG_LOG("key = %d, scancode = %d, action = %d, mods = %d", key, scancode, action, mods);
+	// TODO: Increase acceleration / velocity
+}
---
+++ Engine/TerrainCollisionComponent.cpp
@@ -0,0 +1,34 @@
+#include "stdafx.h"
+#include "TerrainCollisionComponent.h"
+
+// TODO: Don't hard-code any value! Ever!
+// TODO: This variable should be moved into the Physics engine and be accessible from anywhere (similarily as stdlog is accessible).
+/* static */ const Math::Vector3D Engine::TerrainCollisionComponent::GRAVITY_ACCELERATION(0.0f, -9.8f, 0.0f);
+
+Engine::TerrainCollisionComponent::TerrainCollisionComponent(Physics::PhysicsObject* physicsObject, Rendering::TerrainMesh* terrainMesh) :
+	GameComponent(),
+	m_physicsObject(physicsObject),
+	m_terrainMesh(terrainMesh),
+	m_isTouchingTerrain(false)
+{
+}
+
+
+Engine::TerrainCollisionComponent::~TerrainCollisionComponent()
+{
+}
+
+void Engine::TerrainCollisionComponent::Update(Math::Real deltaTime)
+{
+	Math::Vector3D& position = GetTransform().GetPos();
+	Math::Real terrainHeight = m_terrainMesh->GetHeightAt(position.GetXZ(), true);
+	if (position.GetY() < terrainHeight)
+	{
+		position.SetY(terrainHeight);
+		if (!m_isTouchingTerrain)
+		{
+			m_physicsObject->ApplyLinearAcceleration(-GRAVITY_ACCELERATION);
+			m_isTouchingTerrain = true;
+		}
+	}
+}
