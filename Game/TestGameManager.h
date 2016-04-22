2e3384f98d21aecbfdbb0f86867d552a263b0580
artulec88 <artulec88@gmail.com>
2016-04-22 18:26:17 +0200

    Effects are now configurable.

--- Game/TestGameManager.h
+++ Game/TestGameManager.h
@@ -64,6 +64,9 @@ private:
 	void AddSpotLights();
 	void AddCameras(Engine::GameNode* entityToFollow);
 	void AddSkybox() const;
+	void AddStaticEffects();
+	void AddSmoothEffects();
+	void AddBlinkEffects();
 /* ==================== Non-static member functions end ==================== */
 
 /* ==================== Non-static member variables begin ==================== */
@@ -85,13 +88,13 @@ protected:
 	SaveGameCommand m_saveGameCommand;
 	LoadGameCommand m_loadGameCommand;
 
-	std::vector<Rendering::Effects::Effect<Math::Real>> m_singeValueStaticEffects;
+	std::vector<Rendering::Effects::Effect<Math::Real>> m_singleValueStaticEffects;
 	std::vector<Rendering::Effects::Effect<Math::Vector2D>> m_vec2DStaticEffects;
 	std::vector<Rendering::Effects::Effect<Math::Vector3D>> m_vec3DStaticEffects;
-	std::vector<Rendering::Effects::SmoothTransitionEffect<Math::Real>> m_singeValueSmoothEffects;
+	std::vector<Rendering::Effects::SmoothTransitionEffect<Math::Real>> m_singleValueSmoothEffects;
 	std::vector<Rendering::Effects::SmoothTransitionEffect<Math::Vector2D>> m_vec2DSmoothEffects;
 	std::vector<Rendering::Effects::SmoothTransitionEffect<Math::Vector3D>> m_vec3DSmoothEffects;
-	std::vector<Rendering::Effects::BlinkEffect<Math::Real>> m_singeValueBlinkEffects;
+	std::vector<Rendering::Effects::BlinkEffect<Math::Real>> m_singleValueBlinkEffects;
 	std::vector<Rendering::Effects::BlinkEffect<Math::Vector2D>> m_vec2DBlinkEffects;
 	std::vector<Rendering::Effects::BlinkEffect<Math::Vector3D>> m_vec3DBlinkEffects;
 
