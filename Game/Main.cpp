10bb6360dc4b6634bdbdc9e2e47fd08e0567a2d8
artulec88 <artulec88@gmail.com>
2016-06-16 11:05:16 +0200

    Refactorization in the Main.

--- Game/Main.cpp
+++ Game/Main.cpp
@@ -1,33 +1,21 @@
-/* MATH begin */
-#include "Math\Math.h"
-#include "Math\Vector.h"
-#include "Math\Angle.h"
-#include "Math\Quaternion.h"
-/* MATH end */
-
-/* UTILITY begin */
+/* UTILITY includes begin */
 #include "Utility\ILogger.h"
 #include "Utility\ICommand.h"
 #include "Utility\IConfig.h"
-/* UTILITY end */
+/* UTILITY includes end */
 
-///* RENDERING begin */
-//#include "Rendering\Renderer.h"
+/* ENGINE includes begin */
 #include "Engine\CoreEngine.h"
-#include "Rendering\Camera.h"
-//#include "Rendering\Vertex.h"
-///* RENDERING end */
+/* ENGINE includes end */
 
+/* GAME includes begin */
 #include "TestGameManager.h"
 #include "Def.h"
+/* GAME includes end */
 
 #ifdef _DEBUG
 //#include <vld.h> // This is going to cause huge (!!!) performance issues, but is very helpful in detecting memory leaks in the application
 #endif
-
-using namespace Math;
-using namespace Utility;
-using namespace Rendering;
 
 void PrintHelp()
 {
@@ -42,33 +30,23 @@ void PrintHelp()
 
 void ReadSettingsAndParameters(int argc, char* argv[], std::string* shaderDirectory, std::string* modelsDirectory, std::string* texturesDirectory, std::string* fontsDirectory, std::string* audioDirectory)
 {
-	std::unique_ptr<ICommand> command = ICommand::CreateCommand(argc, argv);
+	std::unique_ptr<Utility::ICommand> command = Utility::ICommand::CreateCommand(argc, argv);
 	if (command->IsPresent("-help"))
 	{
 		PrintHelp();
 		system("pause");
 		exit(EXIT_SUCCESS);
 	}
-	IConfig::CreateConfig("Audio", command->Get("-configAudio", "..\\Config\\ConfigAudio.cfg"));
-	IConfig::CreateConfig("Physics", command->Get("-configPhysics", "..\\Config\\ConfigPhysics.cfg"));
-	IConfig::CreateConfig("Rendering", command->Get("-configRendering", "..\\Config\\ConfigRendering.cfg"));
-	IConfig::CreateConfig("Engine", command->Get("-configEngine", "..\\Config\\ConfigEngine.cfg"));
-	IConfig::CreateConfig("Game", command->Get("-configGame", "..\\Config\\ConfigGame.cfg"));
-	IConfig::CreateConfig("Math", command->Get("-configMath", "..\\Config\\ConfigMath.cfg"));
-	IConfig::CreateConfig("Utility", command->Get("-configUtility", "..\\Config\\ConfigUtility.cfg"));
+	Utility::IConfig::CreateConfig("Audio", command->Get("-configAudio", "..\\Config\\ConfigAudio.cfg"));
+	Utility::IConfig::CreateConfig("Physics", command->Get("-configPhysics", "..\\Config\\ConfigPhysics.cfg"));
+	Utility::IConfig::CreateConfig("Rendering", command->Get("-configRendering", "..\\Config\\ConfigRendering.cfg"));
+	Utility::IConfig::CreateConfig("Engine", command->Get("-configEngine", "..\\Config\\ConfigEngine.cfg"));
+	Utility::IConfig::CreateConfig("Game", command->Get("-configGame", "..\\Config\\ConfigGame.cfg"));
+	Utility::IConfig::CreateConfig("Math", command->Get("-configMath", "..\\Config\\ConfigMath.cfg"));
+	Utility::IConfig::CreateConfig("Utility", command->Get("-configUtility", "..\\Config\\ConfigUtility.cfg"));
 
-	/* ==================== Initializing logger begin ==================== */
-	std::string loggingLevel = "Info";
-	if (command->IsPresent("-log"))
-	{
-		loggingLevel = command->Get("-log", "Info");
-	}
-	else
-	{
-		loggingLevel = GET_CONFIG_VALUE_STR_GAME("LoggingLevel", "Info");
-	}
-	ILogger::GetLogger("Game").Fill(loggingLevel, Info);
-	/* ==================== Initializing logger end ==================== */
+	// Initializing logger
+	Utility::ILogger::GetLogger("Game").Fill((command->IsPresent("-log")) ? command->Get("-log", "Info") : GET_CONFIG_VALUE_STR_GAME("LoggingLevel", "Info"), Utility::Info);
 
 	// Initializing shader directory
 	*shaderDirectory = (command->IsPresent("-shaders")) ? command->Get("-shaders", "..\\Shaders\\") : GET_CONFIG_VALUE_STR_GAME("shadersDirectory", "..\\Shaders\\");
@@ -95,9 +73,9 @@ int main(int argc, char* argv[])
 	std::string shaderDirectory, modelsDirectory, texturesDirectory, fontsDirectory, audioDirectory;
 	ReadSettingsAndParameters(argc, argv, &shaderDirectory, &modelsDirectory, &texturesDirectory, &fontsDirectory, &audioDirectory);
 	/* ==================== Create game instance and run ==================== */
-	std::string windowTitle = GET_CONFIG_VALUE_STR_GAME("windowTitle", "Default window title");
 	Engine::CoreEngine engine(GET_CONFIG_VALUE_GAME("windowWidth", 1024), GET_CONFIG_VALUE_GAME("windowHeight", 600),
-		windowTitle.c_str(), GET_CONFIG_VALUE_GAME("FPScap", 200), shaderDirectory, modelsDirectory, texturesDirectory, fontsDirectory, audioDirectory);
+		GET_CONFIG_VALUE_STR_GAME("windowTitle", "Default window title").c_str(), GET_CONFIG_VALUE_GAME("FPScap", 200),
+		shaderDirectory, modelsDirectory, texturesDirectory, fontsDirectory, audioDirectory);
 	Game::TestGameManager game;
 	engine.Start(&game);
 	return EXIT_SUCCESS;
