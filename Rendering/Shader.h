c14aeb090c1b000f501899cd5fd576c0017fd5ba
artulec88 <artulec88@gmail.com>
2016-08-10 14:29:51 +0200

    Shader class comments.

--- Rendering/Shader.h
+++ Rendering/Shader.h
@@ -120,43 +120,95 @@ namespace Rendering
 		/* ==================== Non-static member variables end ==================== */
 	}; /* end class ShaderData */
 
+	/// <summary>
+	/// The class representing the shaders.
+	/// </summary>
+	/// <remarks>
+	/// See https://en.wikipedia.org/wiki/Shader.
+	/// </remarks>
 	class Shader
 	{
 		/* ==================== Static variables begin ==================== */
 	private:
-		//static std::map<std::string, std::shared_ptr<ShaderData>> shaderResourceMap;
 		/* ==================== Static variables end ==================== */
 
 		/* ==================== Constructors and destructors begin ==================== */
 	public:
+		/// <summary> Shader constructor. </summary>
+		/// <param name="fileName"> The file name which contains the shader's code. </param>
 		RENDERING_API Shader(const std::string& fileName);
+		/// <summary> Shader destructor. </summary>
 		RENDERING_API ~Shader(void);
 
+		/// <summary> Shader copy constructor. </summary>
 		Shader(const Shader& shader) = delete;
+		/// <summary> Shader move constructor. </summary>
 		RENDERING_API Shader(Shader&& shader);
+		/// <summary> Shader copy assignment operator. </summary>
 		Shader& operator=(const Shader& shader) = delete;
+		/// <summary> Shader move assignment operator. </summary>
 		Shader& operator=(Shader&& shader) = delete;
 		/* ==================== Constructors and destructors end ==================== */
 
 		/* ==================== Non-static member functions begin ==================== */
 	public:
+		/// <summary> Binds the shader's program. </summary>
 		void Bind() const;
+		/// <summary> Checks whether the shader's program is currently bound. </summary>
+		/// <returns> <code>true</code> if shader's program is bound and <code>false</code> otherwise. </returns>
 		bool IsBound() const;
+		/// <summary> Unbinds the shader's program. </summary>
 		void Unbind() const;
+		/// <summary> Updates the uniforms which come from the rendering engine itself. </summary>
+		/// <param name="renderer"> The rendering engine that defines the values for the uniforms. </param>
 		void UpdateRendererUniforms(const Renderer* renderer) const;
+		/// <summary> Updates the uniforms which come from the material. </summary>
+		/// <param name="material"> The material that defines the values for the uniforms. </param>
 		void UpdateMaterialUniforms(const Material* material) const;
+		/// <summary> Updates the uniforms which come from the transform object. </summary>
+		/// <param name="transform"> The transform that defines the values for the uniforms. </param>
 		void UpdateTransformUniforms(const Math::Transform& transform) const;
+		/// <summary> Updates all shader's uniforms. </summary>
+		/// <param name="transform"> The transform that defines the values for the uniforms. </param>
+		/// <param name="material"> The material that defines the values for the uniforms. </param>
+		/// <param name="renderer"> The rendering engine that defines the values for the uniforms. </param>
 		void UpdateUniforms(const Math::Transform& transform, const Material* material, const Renderer* renderer) const;
 
+		/// <summary> Updates the integer uniform with the new value. </summary>
+		/// <param name="uniformName"> The name of the uniform. </param>
+		/// <param name="value"> The new integer value for the uniform. </param>
 		void SetUniformi(const std::string& uniformName, int value) const;
+		/// <summary> Updates the floating-point uniform with the new value. </summary>
+		/// <param name="uniformName"> The name of the uniform. </param>
+		/// <param name="value"> The new floating-point value for the uniform. </param>
 		void SetUniformf(const std::string& uniformName, Math::Real value) const;
+		/// <summary> Updates the <code>Vector2D</code> uniform with the new value. </summary>
+		/// <param name="uniformName"> The name of the uniform. </param>
+		/// <param name="vector"> The new <code>Vector2D</code> value for the uniform. </param>
 		void SetUniformVector2D(const std::string& uniformName, const Math::Vector2D& vector) const;
+		/// <summary> Updates the <code>Vector3D</code> uniform with the new value. </summary>
+		/// <param name="uniformName"> The name of the uniform. </param>
+		/// <param name="vector"> The new <code>Vector3D</code> value for the uniform. </param>
 		void SetUniformVector3D(const std::string& uniformName, const Math::Vector3D& vector) const;
+		/// <summary> Updates the <code>Vector4D</code> uniform with the new value. </summary>
+		/// <param name="uniformName"> The name of the uniform. </param>
+		/// <param name="vector"> The new <code>Vector4D</code> value for the uniform. </param>
 		void SetUniformVector4D(const std::string& uniformName, const Math::Vector4D& vector) const;
+		/// <summary> Updates the <code>Vector4D</code> uniform with the new value. </summary>
+		/// <param name="uniformName"> The name of the uniform. </param>
+		/// <param name="x"> The new floating-point value for the uniform X component of the <code>Vector4D</code> uniform. </param>
+		/// <param name="y"> The new floating-point value for the uniform Y component of the <code>Vector4D</code> uniform. </param>
+		/// <param name="z"> The new floating-point value for the uniform Z component of the <code>Vector4D</code> uniform. </param>
+		/// <param name="w"> The new floating-point value for the uniform W component of the <code>Vector4D</code> uniform. </param>
 		void SetUniformVector4D(const std::string& uniformName, Math::Real x, Math::Real y, Math::Real z, Math::Real w) const;
+		/// <summary> Updates the <code>Color</code> uniform with the new value. </summary>
+		/// <param name="uniformName"> The name of the uniform. </param>
+		/// <param name="color"> The new <code>Color</code> value for the uniform. </param>
 		void SetUniformColor(const std::string& uniformName, const Color& color) const;
+		/// <summary> Updates the <code>Matrix4D</code> uniform with the new value. </summary>
+		/// <param name="uniformName"> The name of the uniform. </param>
+		/// <param name="matrix"> The new <code>Matrix4D</code> value for the uniform. </param>
 		void SetUniformMatrix(const std::string& uniformName, const Math::Matrix4D& matrix) const;
-	protected:
 	private:
 		GLuint GetProgramID() const { return m_shaderData.GetProgram(); }
 		/// <summary>
@@ -176,7 +228,7 @@ namespace Rendering
 		/* ==================== Non-static member functions end ==================== */
 
 		/* ==================== Non-static member variables begin ==================== */
-	protected:
+	private:
 		ShaderData m_shaderData;
 		std::string m_fileName;
 		/* ==================== Non-static member variables end ==================== */
