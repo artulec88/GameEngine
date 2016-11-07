4b15e2eb365e75d9bf0646cb877ed7e1f902680b
artulec88 <artulec88@gmail.com>
2016-11-07 14:51:28 +0100

    Updated the script that counts the number of lines in code.

--- wc.bat
+++ wc.bat
@@ -1,7 +1,7 @@
 @echo off
 setlocal
 if not [%1]==[] pushd %1
-for /r %%F in (GameEngine\*.h GameEngine\*.cpp Engine\*.h Engine\*.cpp Math\*.h Math\*.cpp MathTest\*.h MathTest\*.cpp Physics\*.h Physics\*.cpp Rendering\*.h Rendering\*.cpp Utility\*.h Utility\*.cpp UtilityTest\*.h UtilityTest\*.cpp) do call :sub "%%F"
+for /r %%F in (GameEngine\*.h GameEngine\*.cpp Engine\*.h Engine\*.cpp Math\*.h Math\*.cpp MathTest\*.h MathTest\*.cpp Physics\*.h Physics\*.cpp Rendering\*.h Rendering\*.cpp RenderingTest\*.h RenderingTest\*.cpp Utility\*.h Utility\*.cpp UtilityTest\*.h UtilityTest\*.cpp) do call :sub "%%F"
 echo Total lines in %Files% files: %Total%
 popd
 exit /b 0
