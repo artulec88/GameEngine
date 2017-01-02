330a4959abc98163fe3297dceedc60a52ba9e6d4
artulec88 <artulec88@gmail.com>
2017-01-02 15:39:56 +0100

    Just refactoring.

--- Utility/Builder.h
+++ Utility/Builder.h
@@ -8,6 +8,9 @@
 
 namespace Utility {
 
+	/// <summary>
+	/// The builder of the object of type <code>T</code>.
+	/// </summary>
 	template <class T>
 	class Builder
 	{
@@ -21,23 +24,38 @@ namespace Utility {
 		explicit Builder()
 		{
 		}
+
 		/// <summary> Builder destructor. </summary>
 		virtual ~Builder(void)
 		{
 		}
+
 		/// <summary> Builder copy constructor. </summary>
-		Builder(Builder& builder) = delete;
+		/// <param name="builder"> The builder to copy construct from. </param>
+		Builder(const Builder& builder) = delete;
+
 		/// <summary> Builder move constructor. </summary>
+		/// <param name="builder"> The r-value reference of the builder to move construct from. </param>
 		Builder(Builder&& builder) = delete;
+
 		/// <summary> Builder copy assignment operator. </summary>
+		/// <param name="builder"> The builder to copy assign from. </param>
+		/// <returns> The newly copy-assigned builder. </returns>
 		Builder& operator=(const Builder& builder) = delete;
+
 		/// <summary> Builder move assignment operator. </summary>
+		/// <param name="builder"> The r-value reference of the builder to move assign from. </param>
+		/// <returns> The newly move-assigned builder. </returns>
 		Builder& operator=(Builder&& builder) = delete;
 		/* ==================== Constructors and destructors end ==================== */
 
 		/* ==================== Non-static member functions begin ==================== */
 	protected:
+		/// <summary> Retrieves the built object. </summary>
+		/// <returns> The built object of type <code>T</code>. </returns>
 		virtual T Get() = 0;
+
+		/// <summary> Builds the object. </summary>
 		virtual void Build() = 0;
 		/* ==================== Non-static member functions end ==================== */
 
